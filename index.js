89199120284d9a390d18da02e19e8a87a2b2e680
NickDub <dubois.nicolas@free.fr>
2019-10-08 13:01:02 +0200

    ewelink-api version bump

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = function (sails) {
-  const install = require('./lib/install.js');
-  const setup = require('./lib/setup.js');
-  const exec = require('./lib/exec.js');
+  const install = require('./lib/install');
+  const setup = require('./lib/setup');
+  const exec = require('./lib/exec');
+  const init = require('./lib/init');
 
   return {
     install,
     setup,
     exec,
+    init,
   };
 };
